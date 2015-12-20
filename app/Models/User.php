88cbb81f5eb902b461c3eab1b1da979282cb548d
Murilo Lima <murilo.wlima@gmail.com>
2015-12-20 18:58:48 -0200

    User model fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ class User extends Model implements AuthenticatableContract
 {
     use Authenticatable;
 
-    protected $fillable = ['name', 'email', 'password', 'avatar'];
-    protected $hidden = ['password', 'roles', 'avatar', 'confirmation', 'status', 'facebook_id'];
+    protected $fillable = ['name', 'email', 'password', 'avatar_id'];
+    protected $hidden = ['password', 'roles', 'avatar_id', 'confirmation', 'status', 'facebook_id'];
 
 }
