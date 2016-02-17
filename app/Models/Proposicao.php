c942468989782792fa6c6c8c411763f537cfc6bc
Murilo Lima <murilo.wlima@gmail.com>
2016-02-17 17:29:28 -0200

    Included remaing columns

--- app/Models/Proposicao.php
+++ app/Models/Proposicao.php
@@ -7,7 +7,7 @@ class Proposicao extends Model
 {
     protected $table = 'proposicoes';
     public $timestamps = false;
-    protected $fillable = ['id', 'parlamentar_id', 'categoria_id', 'ementa', 'resumo', 'nome', 'camara_id', 'situacao', 'descricao', 'colaborador_id'];
+    protected $fillable = ['id', 'tipo', 'codigo', 'parlamentar_id', 'categoria_id', 'ementa', 'resumo', 'nome', 'camara_id', 'situacao', 'descricao', 'colaborador_id'];
 
     public function votos()
     {
