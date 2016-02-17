3293b9e5dfcc20d38605c20892aac2a02601151d
Murilo Lima <murilo.wlima@gmail.com>
2016-02-17 17:19:31 -0200

    Added proposicoes to public route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,13 +8,15 @@ Route::group(['prefix' => 'api/v1/'], function () {
 	Route::group(['as' => 'api_auth'], function () {
 		Route::post('auth', ['uses' => 'AuthController@authenticate']);
 	    Route::post('auth/{provider}', ['uses' => 'AuthController@oAuth']);
+        Route::get('proposicoes/{user_id}', 'ProposicoesController@index');
+        Route::post('proposicoes/votar/{id}', 'ProposicoesController@votar');    
 	});	
+
     
 
     Route::group(['middleware' => 'jwt.auth'], function () {
         Route::get('authenticate/user', 'AuthController@getAuthenticatedUser');
         Route::get('users', 'UsersController@getIndex');
-        Route::get('proposicoes/{user_id}', 'ProposicoesController@index');
-        Route::post('proposicoes/votar/{id}', 'ProposicoesController@votar');
+
     });
 });
