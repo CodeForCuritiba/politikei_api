839d73e9b68c46470191c3abb9b7fc9e4623b649
rennanfelipe <rennan.chaves95@gmail.com>
2016-08-17 21:33:36 -0300

    Models name are fixed

--- app/VotosUsers.php
+++ app/VotosUsers.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class VotoUser extends Model
+class VotosUsers extends Model
 {
     public $timestamps  = false;
     protected $fillable = ['user_id', 'proposicao_id', 'voto'];
