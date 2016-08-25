cbd22297a6b4079a928c4deca48e17e39db55f1f
Murilo Lima <murilo.wlima@gmail.com>
2016-08-24 23:39:21 -0300

    Add route auth protection

--- config/jwt.php
+++ config/jwt.php
@@ -21,7 +21,7 @@ return [
     |
     */
 
-    'secret' => env('JWT_SECRET', 'changeme'),
+    'secret' => env('JWT_SECRET', 'XyGh92i8Bs9QLzRG3zxLWZk48M4gE1th'),
 
     /*
     |--------------------------------------------------------------------------
