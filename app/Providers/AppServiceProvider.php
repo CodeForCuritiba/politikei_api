8951344bd49dd2646279a82e42026957128dda0d
Murilo Lima <murilo.wlima@gmail.com>
2016-08-26 21:29:16 -0300

    [fix] lumen bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,16 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\ResponseFactory;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
+ 	/**
      * Register any application services.
      *
      * @return void
      */
     public function register()
     {
-        //
+        $this->app->singleton('Illuminate\Contracts\Routing\ResponseFactory', function ($app)
+        {
+            return new ResponseFactory($app['Illuminate\Contracts\View\Factory'], $app['Illuminate\Routing\Redirector']);
+        });
     }
 }
